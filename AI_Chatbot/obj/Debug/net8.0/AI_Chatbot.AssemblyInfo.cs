41f7143fa36dd8220d6d0dfd24a2a36c690f9538
Shyam-Mashru <mashrushyam030303@outlook.com>
2025-03-05 23:46:12 +0530

    Adding Appsetting.json folder

--- AI_Chatbot/obj/Debug/net8.0/AI_Chatbot.AssemblyInfo.cs
+++ AI_Chatbot/obj/Debug/net8.0/AI_Chatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Chatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c05eb8ceb756b178ec35b9765e6ba2a2f6da179")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358b6802c3f48caf6fff3c684e86f75cd6b61fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Chatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Chatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
